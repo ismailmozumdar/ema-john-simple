a04cf370a83276014debf0df98a66df81108744f
ismailmozumdar <ismailmozumdar770@gmail.com>
2022-03-24 14:49:12 +0600

    first commit

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoryes } from '../../utilities/fakedb';
 import Cart from '../cadt/cart';
 import Product from '../Product-curd/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [product, setProduct] = useState([]);
+    const [products, setProduct] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProduct(data))
     }, [])
+    useEffect(() => {
+        const storedCart = getStoryes();
+        for(const id in  storedCart){
+        const addProduct = products.find(product => product.id === id);
+        console.log(addProduct)
+    }
+    }, [products])
     const handelAddtocart = (product) =>{
         const newCart = [...cart, product]
         setCart(newCart)
+        addToDb(product.id)
     }
+
 
     return (
         <div className='Shop-container'>
             <div className='Product-container'>
             {
-                product.map(product=> <Product
+                products.map(product=> <Product
                     key={product.id}
                     product={product}
                     handelAddtocart={handelAddtocart}
                     ></Product>)
             }
             </div>
-            <div className='caurd-container App'>
+            <div className='caurd-container'>
             <Cart cart={cart}></Cart>
             </div>
         </div>
--- src/Component/cadt/cart.js
+++ src/Component/cadt/cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './cart.css'
 
 const Cart = ({cart}) => {
+    let total =0;
+    let shipping =0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tex = parseFloat((total * 0.1).toFixed(2))
+    const GrantTotal = total + shipping + tex;
     return (
-        <div>
+        <div className='cart'>
             <h4>caurd-Information</h4>
             <p>select item: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping:${shipping}</p>
+            <p>Tex: ${tex}</p>
+            <h4>Grant Total: ${GrantTotal.toFixed(2)}</h4>
         </div>
     );
 };
